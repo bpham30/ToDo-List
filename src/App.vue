1e7a9c940ce03788c3f01b8f74c516f84e0b9816
Bang <bangpham2701@gmail.com>
2019-07-25 21:15:07 -0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+	<div id="app">
+		<Header v-on:createTodo="createTodo" />
+		<Todos v-bind:todos="todos" v-on:removeTodo="removeTodo"/>
+	</div>
+</template>
+
+<script>
+	import Header from '@/components/Header.vue';
+	import Todos from '@/components/Todos.vue';
+
+	let todos = [
+		{
+			id: "1",
+			name: "Program",
+			done: false
+		},
+		{
+			id: "2",
+			name: "SAT Practice",
+			done: false
+		},
+		{
+			id: "3",
+			name: "Drive",
+			done: false
+		},
+		{
+			id: "4",
+			name: "Run",
+			done: false
+		},
+		{
+			id: "5",
+			name: "Party",
+			done:false
+		}
+	];
+
+	export default {
+		name: 'app',
+		components: {
+			Header,
+			Todos
+		},
+		data: function () {
+			return {
+				todos: todos
+			}
+		},
+		methods: {
+			createTodo: function (todo) {
+				let todoObject = {
+					id: Math.floor(Math.random() * 10000000),
+					name: todo,
+					done: false
+				}
+
+				this.todos.push(todoObject);
+			},
+			removeTodo: function (id) {
+				this.todos = this.todos.filter(todo => todo.id != id)
+			}
+		}
+	}
+
+</script>
+
+<style lang="scss">
+body{
+	margin: 0;
+	padding: 0;
+	background-color: #f3f3f3;
+
+	font-family: Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,68 @@
+<template>
+    <section id="todos">
+        <div class="todo" v-for="(todo) in todos" v-bind:key="todo.id">
+            <input type="checkbox" name="done" v-if="todo.done" checked>
+            <input type="checkbox" name="done" v-else>
+            <label for="done">
+                 {{ todo.name }}
+            </label>
+            <button class ="remove" v-on:click="() => removeTodo(todo.id)">Remove</button>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name:"Todos",
+    props: ["todos"],
+    methods: {
+        removeTodo: function (id) {
+            this.$emit("removeTodo", id);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    #todos {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        flex-wrap: wrap;
+        align-items: center;
+
+        width: 100%;
+
+        .todo {
+            width: 24%;
+            margin: 15px;
+            padding: 15px;
+            background: white;
+
+            display: flex;
+
+            label {
+                flex: 1;
+            }
+            button {
+            color: #171717;
+           line-height: 1px;
+           font-weight: 700;
+
+           background-color: #FFCE00;
+           border: none;
+           outline: none;
+
+           cursor: pointer;
+
+           &:hover {
+               background-color: lighten($color: #FFCE00, $amount: 10)
+            }
+           &:active {
+               background-color: darken($color: #FFCE00, $amount: 5)
+            }
+        }
+    }
+}
+</style>
+
